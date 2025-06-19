31f20e175052a09e8e530b275964a064d63c43fd
Shakir Hussayn <muhammadaaqib.sthps22@ibacc.edu.pk>
2025-06-19 10:44:40 +0500

    Fix: Increase password column size

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ from . import db  # <-- THIS IS THE CORRECTED LINE
 class User(UserMixin, db.Model):
     id       = db.Column(db.Integer, primary_key=True)
     email    = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
     role     = db.Column(db.String(20), default='staff')
 
     def set_password(self, pwd):
